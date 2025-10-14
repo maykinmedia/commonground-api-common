8061f7aa37c9e03d03ab52db68e6120a5998a88a
Steven Bal <steven@maykinmedia.nl>
2025-10-14 12:21:55 +0200

    :white_check_mark: [open-zaak/open-zaak#1878] Add tests for gegevensgroepen
    * to check serializing them as null
    * to check that choicefields do not accept blank
    * to check that charfields allow blank correctly

--- tests/test_gegevensgroepen.py
+++ tests/test_gegevensgroepen.py
@@ -1,7 +1,12 @@
+import pytest
 from rest_framework.test import APIRequestFactory
 
-from testapp.models import Group, Person
-from testapp.serializers import PersonSerializer, PersonSerializer2
+from testapp.models import FkModel, Group, Person
+from testapp.serializers import (
+    FkModelSerializer,
+    PersonSerializer,
+    PersonSerializer2,
+)
 
 
 def test_partial_serializer_validation_gegevensgroep_invalid():
@@ -93,14 +98,109 @@ def test_assignment_missing_optional_key():
     assert group.subgroup_field_2 == "baz"
 
 
-def test_nullable_gegevensgroep():
+def test_nullable_and_empty_gegevensgroep_with_fields_that_do_not_allow_blank_returned_as_null():
     person = Person(name="bla", address_street="", address_number="")
     factory = APIRequestFactory()
     request = factory.get("/persons")
     output = PersonSerializer(context={"request": request}).to_representation(
         instance=person
     )
+    assert output["address"] is None
+
+
+def test_nullable_and_not_fully_empty_gegevensgroep_with_fields_that_do_not_allow_blank_not_returned_as_null():
+    person = Person(name="bla", address_street="kerkstraat", address_number="")
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    output = PersonSerializer(context={"request": request}).to_representation(
+        instance=person
+    )
     assert output["address"] == {
+        "street": "kerkstraat",
+        "number": "",
+    }
+
+
+def test_not_nullable_and_empty_gegevensgroep_not_returned_as_null():
+    person = Person(name="bla", address_street="", address_number="")
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    output = PersonSerializer2(context={"request": request}).to_representation(
+        instance=person
+    )
+    assert output["address_not_null"] == {
         "street": "",
         "number": "",
     }
+
+
+def test_not_nullable_and_not_empty_gegevensgroep_not_returned_as_null():
+    person = Person(name="bla", address_street="", address_number="10")
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    output = PersonSerializer2(context={"request": request}).to_representation(
+        instance=person
+    )
+    assert output["address_not_null"] == {
+        "street": "",
+        "number": "10",
+    }
+
+
+@pytest.mark.django_db
+def test_nullable_and_empty_gegevensgroep_with_fields_that_allow_blank_not_returned_as_null():
+    person = FkModel(pk=1)
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    output = FkModelSerializer(context={"request": request}).to_representation(
+        instance=person
+    )
+    assert output["gegevensgroep_allow_blank"] == {
+        "attribute1": "",
+    }
+
+
+@pytest.mark.django_db
+def test_nullable_and_not_fully_empty_gegevensgroep_with_fields_that_allow_blank_not_returned_as_null():
+    person = FkModel(pk=1, attribute1="foo")
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    output = FkModelSerializer(context={"request": request}).to_representation(
+        instance=person
+    )
+    assert output["gegevensgroep_allow_blank"] == {
+        "attribute1": "foo",
+    }
+
+
+def test_required_choicefields_do_not_allow_blank():
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    serializer = FkModelSerializer(
+        context={"request": request}, data={"gegevensgroep": {"attribute2": ""}}
+    )
+
+    assert not serializer.is_valid()
+    assert serializer.errors["gegevensgroep"]["attribute2"][0].code == "invalid_choice"
+
+
+def test_optional_choicefields_allow_blank():
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    serializer = FkModelSerializer(
+        context={"request": request},
+        data={"gegevensgroep_optional": {"attribute2": ""}},
+    )
+
+    assert serializer.is_valid()
+
+
+def test_required_charfields_allow_blank():
+    factory = APIRequestFactory()
+    request = factory.get("/persons")
+    serializer = FkModelSerializer(
+        context={"request": request},
+        data={"gegevensgroep": {"attribute1": "", "attribute2": "op1"}},
+    )
+
+    assert serializer.is_valid()
